4b212c2cad97a60b8e572e48112b25216feed18b
Stephen Hardisty <shardisty@etsy.com>
2012-02-21 21:58:36 -0500

    added some method hints

--- spec/find_listing_spec.rb
+++ spec/find_listing_spec.rb
@@ -1,3 +1,10 @@
+# Some helpful methods...
+# fill_in 'field', :with => 'search term'
+# click_button 'text on button'
+# click_link 'link text'
+# find('.css #string').should have_content('abc')
+# page.should have_content('abc')
+
 describe 'Navigating' do
   before do
     visit '/' # This is relative to Capybara.app_host (set in spec_helper.rb as "http://www.etsy.com")
