9613ab703cdf54074199780a109f826ad55de020
Stephen Hardisty <shardisty@etsy.com>
2012-02-27 19:04:12 -0500

    this is the secret solution. sshhhhh.

--- spec/find_listing_spec.rb
+++ spec/find_listing_spec.rb
@@ -5,12 +5,20 @@
 # find('.css #string').should have_content('abc')
 # page.should have_content('abc')
 
+#describe 'Navigating', :mechanize => true do
 describe 'Navigating' do
   before do
     visit '/' # This is relative to Capybara.app_host (set in spec_helper.rb as "http://www.etsy.com")
+    fill_in 'search_query', :with => 'gold foil origami butterflies'
+    click_button 'search_submit'
   end
 
-  it 'should blah blah blah' do
-    true.should == false
+  it 'should have search results for gold foil origami butterflies' do
+    page.should have_content('gold foil origami butterflies')
+  end
+
+  it 'should have a ship-to address' do
+    click_link 'Gold Foil Origami Butterfly'
+    find('#item-shipping').should have_content('United States')
   end
 end
